1d41c60ec1ba8caaba05a65270cd86110a17339b
Unknown <jefseaff@gmail.com>
2017-09-07 01:47:49 -0700

    Test with 3D elements
    3D design didn't seem to work as well as planned, switching to 2D

---
+++ Project - Shift/Assets/Levels/FixedCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+
+    Vector3 cameraOffset = new Vector3(0,0,-10f);
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //keep camera in player's position
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
